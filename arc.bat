f9760f48959c3ef3c3304c1c6825580a8c55bdb8
Pauan <pcxunlimited@gmail.com>
2013-12-06 09:01:16 +0800

    .

--- arc.bat
+++ arc.bat
@@ -23,6 +23,7 @@ pushd .
 
 ::  http://stackoverflow.com/questions/3827567/how-to-get-the-path-of-the-batch-script-in-windows
 ::  Actually executes Arc
+echo dir /b /s Racket.exe
 echo "%~dp0arc"
 racket "%~dp0arc"
 
