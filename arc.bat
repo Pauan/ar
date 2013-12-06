cf823585592a4b55bfb18a64cd5b5553a9f02e2f
Pauan <pcxunlimited@gmail.com>
2013-12-06 08:59:05 +0800

    .

--- arc.bat
+++ arc.bat
@@ -1,41 +1,41 @@
-rem  @echo off
+::  @echo off
 
-rem  Blatantly stolen and modified from https://gist.github.com/rocketnia/576688
+::  Blatantly stolen and modified from https://gist.github.com/rocketnia/576688
  
-rem  Lines beginning with "rem", like this one, are comments.
+::  Lines beginning with "rem", like this one, are comments.
  
-rem  The "@echo off" line above stops the following commands,
-rem  including these comments, from being displayed to the terminal
-rem  window. For a more transparent view of what this batch file is
-rem  doing, you can take out that line.
+::  The "@echo off" line above stops the following commands,
+::  including these comments, from being displayed to the terminal
+::  window. For a more transparent view of what this batch file is
+::  doing, you can take out that line.
  
-rem  The @ at the beginning of "@echo off" causes that command to be
-rem  invisible too.
+::  The @ at the beginning of "@echo off" causes that command to be
+::  invisible too.
  
  
-rem  Now we'll keep track of ".", which is the current working
-rem  directory, and return to that directory later on using "popd".
-rem  This is mostly useful if we're running this batch file as part of
-rem  a longer terminal session, so that when we exit Arc and return to
-rem  the command prompt we're in the same directory we left.
+::  Now we'll keep track of ".", which is the current working
+::  directory, and return to that directory later on using "popd".
+::  This is mostly useful if we're running this batch file as part of
+::  a longer terminal session, so that when we exit Arc and return to
+::  the command prompt we're in the same directory we left.
  
 pushd .
 
-rem  http://stackoverflow.com/questions/3827567/how-to-get-the-path-of-the-batch-script-in-windows
-rem  Actually executes Arc
-echo "%~dp0\arc"
-Racket "%~dp0\arc"
+::  http://stackoverflow.com/questions/3827567/how-to-get-the-path-of-the-batch-script-in-windows
+::  Actually executes Arc
+echo "%~dp0arc"
+racket "%~dp0arc"
 
-rem  The "pause" command displays a "press any key" message. If Racket
-rem  exits with an error, this command keeps the batch script running
-rem  long enough for you to read the error message. (Double-clicking a
-rem  batch file opens a window that closes once the script is
-rem  complete.)
+::  The "pause" command displays a "press any key" message. If Racket
+::  exits with an error, this command keeps the batch script running
+::  long enough for you to read the error message. (Double-clicking a
+::  batch file opens a window that closes once the script is
+::  complete.)
  
 pause
  
  
-rem  Finally, as planned, we restore the working directory we started
-rem  with.
+::  Finally, as planned, we restore the working directory we started
+::  with.
  
 popd
